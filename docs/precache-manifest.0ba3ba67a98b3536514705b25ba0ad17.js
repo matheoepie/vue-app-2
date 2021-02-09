b80b001dad56632fc467b564e79982769b4ebd2e
matheoepie <matheoepie@outlook.com>
2021-02-09 14:40:36 +0100

    Initial commit

---
+++ docs/precache-manifest.0ba3ba67a98b3536514705b25ba0ad17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d127c7d716daea8482bb",
+    "url": "/vue-app-2/css/app.b229284b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "eb6d58c6178fe220b4497f97a8b1a320",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "d127c7d716daea8482bb",
+    "url": "/vue-app-2/js/app.647cbaa0.js"
+  },
+  {
+    "revision": "3a6798fe18ab96b859e8",
+    "url": "/vue-app-2/js/chunk-vendors.ca792762.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
---
+++ src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <h1>This is an Test page</h1>
+    <TestComponnent msg="TestOuia"/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import TestComponnent from '@/components/TestComponnent.vue'
+
+export default {
+  name: 'test',
+  components: {
+    TestComponnent
+  }
+}
+</script>
